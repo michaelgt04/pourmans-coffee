de499e2473a19dad6fd5fb002bb063ebaa235255
Amanda Beiner <aebeiner@gmail.com>
2017-05-29 21:25:52 -0400

    model and api tests

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-  validates :day, presence: true, inclusion: { in: %w(Monday Tuesday Wednesday Thursday Friday Saturday Sunday)}, case_sensitive: false
+  validates :day, presence: true, inclusion: { in: %w(Monday Tuesday Wednesday Thursday Friday Saturday Sunday)}
 
   validates :start_time, presence: true
   validates :end_time, presence: true
---
+++ spec/models/event_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Event, type: :model do
+
+  it { should have_valid(:day).when("Monday", "Tuesday", "Thursday") }
+  it { should_not have_valid(:day).when(nil, "never", "forever") }
+
+  it { should have_valid(:start_time).when("12:00", "5:00") }
+  it { should_not have_valid(:start_time).when(nil) }
+
+  it { should have_valid(:end_time).when("12:00", "5:00") }
+  it { should_not have_valid(:end_time).when(nil) }
+
+  it { should have_valid(:location).when("Dewey Square", "Post Office Square") }
+  it { should_not have_valid(:location).when(nil) }
+end
