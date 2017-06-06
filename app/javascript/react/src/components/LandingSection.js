d985169119490b8c27383d72654d7f14c045c276
Mike Thomson <michaelgt04@gmail.com>
2017-06-06 18:41:23 -0400

    Initial setup to get landing styling up

---
+++ app/javascript/react/src/components/LandingSection.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class LandingSection extends Component {
+
+  render(){
+    return(
+      <div className='landing-section '>
+        <img className='logo' src='https://res.cloudinary.com/duor0bzmo/image/upload/v1496107389/15940964_1282078901853465_5450203523514265901_n_kpswaj.png' />
+        <h1>Cold Brew</h1>
+      </div>
+    )
+  }
+}
+
+export default LandingSection;
