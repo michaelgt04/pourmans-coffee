67b8893b5018848b2bc8f4bd640ac5feabbed32e
michaelgt04 <michaelgt04@gmail.com>
2017-06-06 21:38:13 -0400

    Added image and basic styling of components for big screen (not responsive)

--- app/javascript/react/src/components/LandingSection.js
+++ app/javascript/react/src/components/LandingSection.js
@@ -5,8 +5,8 @@ class LandingSection extends Component {
   render(){
     return(
       <div className='landing-section '>
-        <img className='logo' src='https://res.cloudinary.com/duor0bzmo/image/upload/v1496107389/15940964_1282078901853465_5450203523514265901_n_kpswaj.png' />
-        <h1>Cold Brew</h1>
+        <img className='logo' src='https://res.cloudinary.com/duor0bzmo/image/upload/v1496790582/Pourman_s_Logo_yzyag0.png' />
+        <i className="fa fa-chevron-down nav-down" aria-hidden="true"></i>
       </div>
     )
   }
