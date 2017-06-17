9fe561eb3ff28658e7c629b207589f6ae1f1d613
Amanda Beiner <aebeiner@gmail.com>
2017-06-16 21:13:32 -0400

    preliminary styling

--- app/javascript/react/src/components/AboutTile.js
+++ app/javascript/react/src/components/AboutTile.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 const AboutTile = props => {
-  debugger;
   let blurb = props.about.blurb
   return(
-    <div>
-      <h2>About Us</h2>
-      <p>{blurb}</p>
+    <div className="overlay">
+      <div className="about-text">
+        <h2>About Us</h2>
+        <p>{blurb}</p>
+      </div>
     </div>
   )
 }
