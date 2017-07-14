31997c75d140e6a89eab3e512c187d37128b2cb7
Amanda Beiner <aebeiner@gmail.com>
2017-07-13 20:44:01 -0400

    test stateless product components

---
+++ app/javascript/react/test/components/ProductTileSpec.js
@@ -0,0 +1,53 @@
+import ProductTile from 'components/ProductTile';
+
+fdescribe('ProductTile', () => {
+  let wrapper,
+      product,
+      handleHover,
+      handleLeave
+
+  product =
+    {
+      "id": 1,
+      "title": "Coconut Cold Brew",
+      "description": "obviously the best",
+      "img_url": "http://nitrocoldbrew.coffeebean.com/images/NitroCB_1920x1080.jpg"
+    }
+
+  handleHover = jasmine.createSpy('handleHover spy')
+  handleLeave = jasmine.createSpy('handleLeave spy')
+
+  beforeEach(() => {
+    wrapper = shallow(
+      <ProductTile
+        key={product.id}
+        product={product}
+        title="Coconut Cold Brew"
+        displayText="obviously the best"
+        handleHover={handleHover}
+        handleLeave={handleLeave}
+      />
+    )
+  })
+
+  it('renders an image with the photo url', () => {
+    expect(wrapper.find('img')).toBePresent();
+    expect(wrapper.find('img')).toHaveProp('src', 'http://nitrocoldbrew.coffeebean.com/images/NitroCB_1920x1080.jpg')
+    console.log(wrapper.debug())
+  })
+
+  it('displays the product description', () => {
+    expect(wrapper.text()).toMatch('obviously the best');
+  })
+
+  it('triggers the handleHover function when the tile is hovered', () => {
+    wrapper.simulate('mouseEnter')
+    expect(handleHover).toHaveBeenCalled()
+  })
+
+  it('triggers the handleLeave function when the tile is unfocused', () => {
+    wrapper.simulate('mouseLeave');
+
+    expect(handleLeave).toHaveBeenCalled();
+  })
+})
