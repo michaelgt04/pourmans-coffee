2f40209bd326eb7ded6de57b8b8bfdcd984c8a78
Mike Thomson <michaelgt04@gmail.com>
2017-07-16 19:58:10 -0400

    Updated products styling for hover

--- app/javascript/react/src/components/ProductTile.js
+++ app/javascript/react/src/components/ProductTile.js
@@ -3,16 +3,16 @@ import React from 'react';
 const ProductTile = props => {
   let product = props.product
   
-  let displayText = product => {
+  let selectedProduct = product => {
     if (product.id === props.selectedProductId){
-      return <p>{product.description}</p>
+      return 'selected'
     } 
   }
 
   return(
     <div onMouseEnter={props.handleHover} onMouseLeave={props.handleLeave} className='product-tile small-10 medium-6 small-offset-1 medium-offset-0 columns end'>
-      <h3>{product.title}</h3>
-      {displayText(product)}
+      <h3 className={selectedProduct(product)}>{product.title}</h3>
+      <p>{product.description}</p>
     </div>
   )
 }
