2090dba78d44cd8128fa673c0255c8b2c165886b
Mike Thomson <michaelgt04@gmail.com>
2017-07-19 18:09:53 -0400

    Added selected scroll for nav bar

--- app/javascript/react/src/components/AnchorLink.js
+++ app/javascript/react/src/components/AnchorLink.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const AnchorLink = props => {
   return(
-    <a className={props.selectedClass} onClick={props.handleClick} href={`${props.anchor}`}>
+    <a className={props.selectedClass} href={`${props.anchor}`}>
       <div>{props.text}</div>
     </a>
   )
