f1ba74252237c28f4b079eeea962c257501e9a30
Mike Thomson <michaelgt04@gmail.com>
2017-07-21 15:11:08 -0400

    Basic router and component setup for Admin interface

---
+++ app/javascript/react/src/admin/containers/AdminDashboardContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class AdminDashboardContainer extends Component{
+  render(){
+    return(
+      <h1>I have Admin Facts</h1>
+    )
+  }
+}
+
+export default AdminDashboardContainer;
