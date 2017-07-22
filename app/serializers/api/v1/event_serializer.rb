78964e832092da333b4b1120334ee743e00ed8bc
Mike Thomson <michaelgt04@gmail.com>
2017-07-22 13:18:14 -0400

    Added testing and post endpoint for events

--- app/serializers/api/v1/event_serializer.rb
+++ app/serializers/api/v1/event_serializer.rb
@@ -1,3 +1,3 @@
 class Api::V1::EventSerializer < ActiveModel::Serializer
-  attributes :day, :start_time, :end_time, :location
+  attributes :id, :day, :start_time, :end_time, :location, :note
 end
--- spec/controllers/api/v1/events_controller_spec.rb
+++ spec/controllers/api/v1/events_controller_spec.rb
@@ -2,7 +2,10 @@ require "rails_helper"
 
 describe Api::V1::EventsController do
   let!(:event) { FactoryGirl.create(:event) }
+  let(:json_parsed_response) { JSON.parse(response.body) }
 
+  let(:correct_event_params) { { day: 'Monday', start_time: '9:00', end_time: '1:00', location: 'Dewey Square', note: 'Every other week' } }
+  let(:incorrect_event_params) { { day: '', start_time: '9:00', end_time: '1:00', location: 'Dewey Square', note: 'Every other week' } }
   describe "#index" do
     let!(:expected_json) {
       {
@@ -11,7 +14,8 @@ describe Api::V1::EventsController do
           "day" => "Tuesday",
           "start_time" => "12:30",
           "end_time" => "1:30",
-          "location" => "Dewey Square"
+          "location" => "Dewey Square",
+          "note" => "Every other week"
         }]
       }
     }
@@ -23,6 +27,30 @@ describe Api::V1::EventsController do
       expect(response.content_type).to eq("application/json")
       expect(response.code).to eq("200")
       expect(parsed).to eq(expected_json)
+    end
+  end
+  
+  describe "POST #create" do
+    it "creates a new event" do
+      expect { post :create, params: { event: correct_event_params } }.to change { Event.count }.by 1
+    end
+
+    it "returns the products as JSON" do
+      post :create, params: { event: correct_event_params }
+
+      expect(response.status).to eq 200
+      expect(response.content_type).to eq("application/json")
+
+      expect(json_parsed_response.keys).to eq ["id", "day", "start_time", "end_time", "location", "note"]
+      expect(json_parsed_response.values).to include("Monday", "9:00", "1:00", "Dewey Square", "Every other week")
+    end
+
+    it "returns an error when the payload is incorrect" do
+      post :create, params: { event: incorrect_event_params }
+
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(json_parsed_response.keys).to eq ["errors"]
+      expect(json_parsed_response["errors"]).to eq ["Day can't be blank", "Day is not included in the list"]
     end
   end
 end
