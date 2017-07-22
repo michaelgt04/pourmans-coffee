713c0368269f0c14acca448d2da45417640df65f
Mike Thomson <michaelgt04@gmail.com>
2017-07-22 14:34:50 -0400

    Added basic show of all admin events

--- app/javascript/react/src/admin/containers/AdminEventsContainer.js
+++ app/javascript/react/src/admin/containers/AdminEventsContainer.js
@@ -1,15 +1,50 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 import AdminEventForm from '../components/AdminEventForm'
+import DayTile from '../components/DayTile';
+import { getEvents } from '../actions/getEvents';
 
 class AdminEventsContainer extends Component{
+  componentDidMount(){
+    this.props.getEvents() 
+  }
+  
   render(){
+    let events = this.props.events
+    let daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+    let days = daysOfWeek.map(day => {
+      return(
+        <DayTile 
+          key={day}
+          day={day}
+          events={events[day]} 
+        /> 
+      ) 
+    })
+
     return(
       <div>
-        <h1>I Will Show Events</h1>
+        <h2>Current Events:</h2>
+        {days}
         <AdminEventForm />
       </div>
     )
   }
 }
 
-export default AdminEventsContainer;
+let mapStateToProps = state => {
+  return{
+    events: state.events 
+  }
+}
+
+let mapDispatchToProps = dispatch => {
+  return{
+    getEvents: () => {
+      dispatch(getEvents()) 
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AdminEventsContainer);
