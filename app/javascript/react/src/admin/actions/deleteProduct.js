c4327553c4f1d026bfdb020f6f88f09a7c06e73d
Amanda Beiner <aebeiner@gmail.com>
2017-07-24 17:20:32 -0400

    delete product functionality working

---
+++ app/javascript/react/src/admin/actions/deleteProduct.js
@@ -0,0 +1,27 @@
+export const DELETE_PRODUCT_SUCCESS = 'DELETE_PRODUCT_SUCCESS';
+
+
+export const deleteProduct = (productId, dispatch) => {
+  return(dispatch) => {
+    fetch(`/api/v1/products/${productId}.json`, {
+      credentials: 'same-origin',
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json'}
+    })
+    .then(response => {
+      let product = response.json()
+      return product;
+    }).then(product => {
+      dispatch(deleteProductSuccess(product))
+    }).catch(error => {
+      console.log(error)
+    })
+  }
+}
+
+export const deleteProductSuccess = (product) => {
+  return {
+    type: DELETE_PRODUCT_SUCCESS,
+    product
+  };
+};
