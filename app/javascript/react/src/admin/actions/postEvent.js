a127bf86161fa061159f92c79cf12076f6b16b72
Mike Thomson <michaelgt04@gmail.com>
2017-07-25 13:27:41 -0400

    Edit form functionality working

--- app/javascript/react/src/admin/actions/postEvent.js
+++ app/javascript/react/src/admin/actions/postEvent.js
@@ -1,21 +1,35 @@
 import { SubmissionError } from 'redux-form';
+import humps from 'humps';
+
+export const POST_EVENT_SUCCESS = 'POST_EVENT_SUCCESS';
+
+export const postEventSuccess = event => {
+  return {
+    type: POST_EVENT_SUCCESS,
+    event
+  }
+}
 
 export const postEvent = payload => {
-  console.log(payload)
-  let body = JSON.stringify(payload)
+  let rubyPayload = humps.decamelizeKeys(payload)
+  let body = JSON.stringify({ event: rubyPayload })
 
-  return fetch('/api/v1/events', {
-    body,
-    method: 'POST'
-  }).then(response => {
-    if(response.ok){
-      return response.json()
-    } else {
-      throw new SubmissionError({ _error: 'It did not work' }) 
-    }
-  }).then(events => {
-    console.log(events) 
-  }).catch(error => {
-    console.log(error) 
-  })
+  return (dispatch, getState) => {
+    fetch('/api/v1/events.json', {
+      body,
+      method: 'POST',
+      credentials: 'same-origin',
+      headers: { 'Content-Type': 'application/json'}
+    }).then(response => {
+      if(response.ok){
+        return response.json()
+      } else {
+        throw new SubmissionError({ _error: 'It did not work' }) 
+      }
+    }).then(event => {
+      dispatch(postEventSuccess(event))
+    }).catch(error => {
+      console.log(error) 
+    })
+  }
 }
