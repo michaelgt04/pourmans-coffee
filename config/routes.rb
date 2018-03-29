6fc10e19df7b5fc620d8416b122fb672a1bc34b8
Mike Thomson <michaelgt04@gmail.com>
2018-03-28 22:16:29 -0400

    Added login gateway to admin

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get "/admin/events" => "admin#index"
   get "/admin/products" => "admin#index"
   get "/admin/about" => "admin#index"
+
+  resources :admin, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api do
